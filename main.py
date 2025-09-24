404d4ff27e3b7910074ec8f0c0d80b4299da0534
zobair11 <zobair619316@gmail.com>
2025-09-23 21:02:16 -0400

    basic llm call

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from dotenv import load_dotenv
+import os
+import sys
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
+
 load_dotenv()
 
-if __name__ == '__main__':
-    print('Welcome to travel helper')
+def ask_llm(query: str, system: str = "You are a concise, helpful assistant.") -> str:
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, api_key=os.getenv("OPENAI_API_KEY"))
+    res = llm.invoke([SystemMessage(content=system), HumanMessage(content=query)])
+    return res.content
+
+if __name__ == "__main__":
+    query = " ".join(sys.argv[1:])
+    print(ask_llm(query))
